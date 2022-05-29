13afccd8248129f60d41b1710488f71c966b44df
akerpelm <akerpelm@u.rochester.edu>
2022-05-29 16:48:09 -0400

    complete register, begin server

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import { Logo, FormRow } from "../components";
+import { Logo, FormRow, Alert } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
+import { useAppContext } from "../context/appContext";
 
 const initialState = {
   name: "",
@@ -13,35 +14,63 @@ const Register = () => {
   const [values, setValues] = useState(initialState);
   // global state and useNavigate
 
+  const { isLoading, showAlert, displayAlert } = useAppContext();
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
   const handleChange = (e) => {
-    console.log(e.target);
+    setValues({ ...values, [e.target.name]: e.target.value });
   };
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!name && !isMember)) {
+      displayAlert();
+      return;
+    }
+    console.log(values);
   };
 
   return (
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>
         <Logo />
-        <h3>Login</h3>
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {showAlert && <Alert />}
         {/* name input */}
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            name
-          </label>
-          <input
+        {!values.isMember && (
+          <FormRow
             type="text"
-            value={values.name}
             name="name"
-            onChange={handleChange}
-            className="form-input"
+            value={values.name}
+            handleChange={handleChange}
+            labelText=""
           />
-          <button type="submit" className="btn btn-block">
-            Submit
+        )}
+        {/* email input */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password input */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
           </button>
-        </div>
+        </p>
       </form>
     </Wrapper>
   );
