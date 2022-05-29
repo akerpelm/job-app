5e111aafff64246b89ffbb4203e42b09d24deacb
akerpelm <akerpelm@u.rochester.edu>
2022-05-29 10:47:53 -0400

    initial commit

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,32 @@
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            vitae! Laboriosam, rem, aspernatur beatae quod pariatur, non modi
+            accusantium vitae accusamus quibusdam nemo. Nisi laboriosam totam
+            cum repudiandae consequuntur accusantium.
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+export default Landing;
