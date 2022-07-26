f327b6edbf3105fd063efbf24d8c2f552f9ebe01
akerpelm <akerpelm@u.rochester.edu>
2022-07-25 22:40:23 -0400

    fix rate limiter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,11 +4,11 @@ import { register, login, updateUser } from "../controllers/authController.js";
 import authenticateUser from "../middleware/auth.js";
 const router = express.Router();
 
-const apiLimit = () => {
-  windowMs: 15 * 60 * 1000;
-  max: 100;
-  message: "Too many requests from this IP address. Please retry in 15 minutes.";
-};
+const apiLimit = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: "Too many requests from this IP, please try again after 15 minutes"
+});
 router.route("/register").post(apiLimit, register);
 router.route("/login").post(apiLimit, login);
 router.route("/updateUser").patch(authenticateUser, updateUser);
